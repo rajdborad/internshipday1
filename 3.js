9732e7b4cc61595cacd5a11b401c5dd296002835
Raj Borad <98679441+rajdborad@users.noreply.github.com>
2022-06-20 17:10:00 +0530

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,63 @@
+function myFunction1() {
+    var dots = document.getElementById("dots");
+    var moreText1 = document.getElementById("more1");
+    var btnText = document.getElementById("myBtn1");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText1.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText1.style.display = "inline";
+    }
+  }
+
+  function myFunction2() {
+    var dots = document.getElementById("dots");
+    var moreText2 = document.getElementById("more2");
+    var btnText = document.getElementById("myBtn2");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText2.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText2.style.display = "inline";
+    }
+  }
+
+  function myFunction3() {
+    var dots = document.getElementById("dots");
+    var moreText3 = document.getElementById("more3");
+    var btnText = document.getElementById("myBtn3");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText3.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText3.style.display = "inline";
+    }
+  }
+
+  function myFunction4() {
+    var dots = document.getElementById("dots");
+    var moreText4 = document.getElementById("more4");
+    var btnText = document.getElementById("myBtn4");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText4.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText4.style.display = "inline";
+    }
+  }
